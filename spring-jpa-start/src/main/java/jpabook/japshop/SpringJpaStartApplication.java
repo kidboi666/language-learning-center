95cf7e260fc38d0f5eb2a0296f672b556847093a
예비 리액트맨 <70879978+kidboi666@users.noreply.github.com>
2024-12-23 11:36:39 +0900

    spring-jpa-start

---
+++ spring-jpa-start/src/main/java/jpabook/japshop/SpringJpaStartApplication.java
@@ -0,0 +1,13 @@
+package jpabook.japshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaStartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJpaStartApplication.class, args);
+  }
+
+}
