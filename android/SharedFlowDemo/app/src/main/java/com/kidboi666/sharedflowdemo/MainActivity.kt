867ef0c6757d5f199589ad55ea86e6c16d82756b
kidboi666 <kidboi666@gmail.com>
2025-08-17 20:52:57 +0900

    셰어드플로우 + 뷰모델

---
+++ android/SharedFlowDemo/app/src/main/java/com/kidboi666/sharedflowdemo/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.kidboi666.sharedflowdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import com.kidboi666.sharedflowdemo.ui.theme.SharedFlowDemoTheme
+import kotlinx.coroutines.flow.SharedFlow
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SharedFlowDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ScreenSetup(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScreenSetup(modifier: Modifier = Modifier, viewModel: DemoViewModel = DemoViewModel()) {
+    Box(modifier = modifier) {
+        MainScreen(viewModel.sharedFlow)
+    }
+}
+
+@Composable
+fun MainScreen(sharedFlow: SharedFlow<Int>) {
+    val messages = remember { mutableStateListOf<Int>() }
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(Unit) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            sharedFlow.collect {
+                println("Collecting $it")
+                messages.add(it)
+            }
+        }
+    }
+    LazyColumn {
+        items(messages) {
+            Text(
+                "Collected Value = $it",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(5.dp)
+            )
+        }
+    }
+    Box {}
+}
