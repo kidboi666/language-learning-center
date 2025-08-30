98b816819a8b286d952870e7df07487920f179d9
kidboi666 <kidboi666@gmail.com>
2025-08-30 15:47:19 +0900

    안드로이드 뷰 SimpleMusicPlayer 앱 마무리

--- android/SimpleMusicPlayer/app/src/main/java/com/logmind/simplemusicplayer/MainActivity.kt
+++ android/SimpleMusicPlayer/app/src/main/java/com/logmind/simplemusicplayer/MainActivity.kt
@@ -5,22 +5,35 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.media.MediaPlayer
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    lateinit var btn_play: Button
+    lateinit var btn_pause: Button
+    lateinit var btn_stop: Button
+    var mService: MusicPlayerService? = null // 뮤직 플레이어 서비스를 담을 변수
+
+    /**
+     * 서비스와 구성요소의 연결 상태를 모니터링 해줌.
+     */
     val mServiceConnection = object : ServiceConnection {
-        override fun onServiceConnected(className: ComponentName?, service: IBinder?) {
-            TODO("Not yet implemented")
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            // MusicPlayerBinder로 형변환 해줌
+            mService = (service as MusicPlayerService.MusicPlayerBinder).getService()
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
-            TODO("Not yet implemented")
+            // 만약 서비스가 끊기면, mService를 null로 만들어줌
+            mService = null
         }
     }
 
@@ -33,20 +46,101 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        playMusic()
+
+        btn_play = findViewById(R.id.btn_play)
+        btn_pause = findViewById(R.id.btn_pause)
+        btn_stop = findViewById(R.id.btn_stop)
+
+        btn_play.setOnClickListener(this)
+        btn_pause.setOnClickListener(this)
+        btn_stop.setOnClickListener(this)
     }
 
-    fun playMusic() {
-        val mPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.chocolate)
-        mPlayer?.start()
-    }
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_play -> {
+                play()
+            }
 
-    fun getUriMusic() {
-        val myUri: Uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.chocolate)
-        val mediaPlayer: MediaPlayer? = MediaPlayer().apply {
-            setDataSource(applicationContext, myUri)
-            prepare()
-            start()
+            R.id.btn_pause -> {
+                pause()
+            }
+
+            R.id.btn_stop -> {
+                stop()
+            }
         }
     }
+
+    /**
+     * [onResume] 콜백 함수는 액티비티가 사용자에게 보이기 시작할때마다 실행되는 콜백 함수이다.
+     * 여기서 바로 서비스를 시작해준다.
+     * [mService] 가 null이면 "아직 서비스가 액티비티와 연결되지 않았다" 는 말이므로 버전에 따라 [startService]
+     * 함수나 [startForegroundService] 함수를 호출해준다.
+     * 이 두 함수는 아무리 많이 불러도 이미 해당 서비스가 만들어진 상태이면 서비스를 새로 만들지 않는다.
+     */
+    override fun onResume() {
+        super.onResume()
+
+        // 서비스 실행
+        if (mService == null) {
+            // 안드로이드 O 이상이면 startForegroundService를 사용해야 함
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(Intent(this, MusicPlayerService::class.java))
+            } else {
+                startService(Intent(applicationContext, MusicPlayerService::class.java))
+            }
+
+
+            // 이 액티비티를 서비스와 바인드 시킴.
+            val intent = Intent(this, MusicPlayerService::class.java)
+
+            /**
+             *
+             * [bindService]
+             *
+             * @param intent 서비스를 실행시키는 인텐트
+             *
+             * @param conn 바인드 시킬 서비스의 콜백 함수
+             * 서비스와 액티비티가 연결되면 [onServiceConnected] 콜백 함수가 호출,
+             * 연결이 끊기면 [onServiceDisconnected] 콜백 함수가 호출된다.
+             *
+             * @param flag 바인드 시킬 때의 플래그
+             * [BIND_AUTO_CREATE] 바인드할 시점에 서비스가 실행되지 않은 상태라면 서비스를 생성
+             */
+            bindService(intent, mServiceConnection, BIND_AUTO_CREATE)
+        }
+    }
+
+    /**
+     *
+     * 사용자가 액티비티를 떠나게 되면 실행되는 콜백함수.
+     * if문을 사용하여 만약 mService가 null이 아닌 상태일 때, 즉 바인딩된 상태일 때, 즉 서비스가 실행된 상태일 때,
+     * 만약 음악이 실행되고 있지 않은 상태라면 서비스를 종료하고 바인딩을 해제한다.
+     * 음악이 실행 중이면 바인딩만 해제하여 다시 앱으로 돌아왔을 때 해당 서비스에 바인딩할 수 있게끔 한다.
+     */
+    override fun onPause() {
+        super.onPause()
+
+        if (mService != null) {
+            if (!mService!!.isPlaying()) {
+                mService!!.stopSelf()
+            }
+            unbindService(mServiceConnection)
+            mService = null
+        }
+    }
+
+    private fun play() {
+        mService?.play()
+    }
+
+    private fun pause() {
+        mService?.pause()
+    }
+
+    private fun stop() {
+        mService?.stop()
+    }
+
 }
--- android/SimpleMusicPlayer/app/src/main/java/com/logmind/simplemusicplayer/MusicPlayerService.kt
+++ android/SimpleMusicPlayer/app/src/main/java/com/logmind/simplemusicplayer/MusicPlayerService.kt
@@ -1,5 +1,6 @@
 package com.logmind.simplemusicplayer
 
+import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -9,7 +10,20 @@ import android.media.MediaPlayer
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
+import android.widget.Toast
 
+/**
+ * 백그라운드 서비스: 사용자가 보이지 않는 곳에서 조용히 작업을 수행함.
+ * 포그라운드 서비스: 사용자가 서비스가 실행되고 있음을 능동적으로 인지할 수 있는 서비스. (예를 들어 음악 재생시 상단 상태 표시줄에 알림 표시)
+ * API 레벨 26(안드로이드 O) 부터는 포그라운드 서비스를 통해 상태 표시줄에서 서비스가 실행되고 있음을 사용자에게 알리고
+ * 다른 서비스보다 높은 우선순위를 가지게끔 함.
+ * [startForegroundService] 함수는 [startService] 와는 다르게 [startForegroundService] 를 호출하고 나서
+ * 서비스 생성 이후 5초 이내에 [startForeground] 함수를 통해 알림을 보여주어야 함.
+ *
+ * @param id 식별자
+ * @param notification 사용자에게 보여질 알림
+ * startForeground(id, notification)
+ */
 class MusicPlayerService : Service() {
 
     var mMediaPlayer: MediaPlayer? = null
@@ -49,26 +63,17 @@ class MusicPlayerService : Service() {
 
     /**
      * [onStartCommand]: [startService] 나 [startForegroundService] 를 호출할 때 실행되는 함수임.
-     * 이 함수가 실행되면 서비스는 시작도니 상태가 되며, 백그라운드에서 쭉 존재할 수 있게 됨
+     * 이 함수가 실행되면 서비스는 시작된 상태가 되며, 백그라운드에서 쭉 존재할 수 있게 됨
+     *
+     * [START_STICKY]: 시스템이 서비스를 중단하면 서비스를 다시 실행하고 [onStartCommand] 함수를 호출한다.
+     *
+     * [START_NOT_STICKY]: 시스템이 서비스를 중단시키면 서비스를 재생성 하지 않음.
+     *
+     * [START_REDELIVER_INTENT]: 시스템이 서비스를 중단하면 서비스를 다시 실행하고 [onStartCommand] 함수를 호출한다.
+     * 또한 서비스가 종료되기 전 마지막으로 전달된 인텐트를 재전달한다. 반드시 명령을 실행해야 하는 경우 쓰임.
      */
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        /**
-         * [START_STICKY]: 시스템이 서비스를 중단하면 서비스를 다시 실행하고 [onStartCommand] 함수를 호출한다.
-         *
-         * [START_NOT_STICKY]: 시스템이 서비스를 중단시키면 서비스를 재생성 하지 않음.
-         *
-         * [START_REDELIVER_INTENT]: 시스템이 서비스를 중단하면 서비스를 다시 실행하고 [onStartCommand] 함수를 호출한다.
-         * 또한 서비스가 종료되기 전 마지막으로 전달된 인텐트를 재전달한다. 반드시 명령을 실행해야 하는 경우 쓰임.
-         */
         return START_STICKY
-    }
-
-    /**
-     * [onDestroy]: 서비스 생명 주기의 마지막 단계임.
-     * 여기서 [onCreate] 에서 상태 표시줄에 보여주었던 알림을 해제함.
-     */
-    override fun onDestroy() {
-        super.onDestroy()
     }
 
     /**
@@ -77,14 +82,17 @@ class MusicPlayerService : Service() {
      * 사용자로 하여금 서비스가 실행되고 있다는 사실을 알림과 함께 알려야 함
      */
     fun startForegroundService() {
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        val mChannel = NotificationChannel(
-            "CHANNEL_ID",
-            "CHANNEL_NAME",
-            NotificationManager.IMPORTANCE_DEFAULT
-        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val mChannel = NotificationChannel(
+                "CHANNEL_ID",
+                "CHANNEL_NAME",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(mChannel)
+        }
 
-        val notification = Notification.Builder(this, "CHANNEL_ID")
+        val notification: Notification = Notification.Builder(this, "CHANNEL_ID")
             .setSmallIcon(R.drawable.ic_play)
             .setContentTitle("뮤직 플레이어 앱")
             .setContentText("앱이 실행 중입니다.")
@@ -93,12 +101,58 @@ class MusicPlayerService : Service() {
         startForeground(1, notification)
     }
 
-    fun isPlaying() {} // 재생 중인지 확인
+    /**
+     * [onDestroy]: 서비스 생명 주기의 마지막 단계임.
+     * 여기서 [onCreate] 에서 상태 표시줄에 보여주었던 알림을 해제함.
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            stopForeground(true)
+        }
+    }
 
-    fun play() {} // 재생
 
-    fun pause() {} // 일시정지
+    fun isPlaying(): Boolean {
+        return mMediaPlayer != null && mMediaPlayer?.isPlaying ?: false
+    }
 
-    fun stop() {} // 완전 정지
+    fun play() {
+        /**
+         * 음악 파일의 리소스를 가져와 미디어 플레이어 객체를 할당해줌.
+         */
+        if (mMediaPlayer == null) {
+            mMediaPlayer = MediaPlayer.create(this, R.raw.chocolate)
+
+            mMediaPlayer?.setVolume(1.0f, 1.0f) // 볼륨 지정
+            mMediaPlayer?.isLooping = true // 반복 재생 여부
+            mMediaPlayer?.start() // 음악 재생
+        } else { // 음악 재생 중인 경우
+            if (mMediaPlayer!!.isPlaying) {
+                Toast.makeText(this, "이미 음악이 실행 중입니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                mMediaPlayer?.start()
+            }
+        }
+    }
+
+    fun pause() {
+        mMediaPlayer?.let {
+            if (it.isPlaying) {
+                it.pause()
+            }
+        }
+    }
+
+    fun stop() {
+        mMediaPlayer?.let {
+            if (it.isPlaying) {
+                it.stop() // 음악을 멈춤
+                it.release() // 미디어 플레이어에 할당된 자원을 해제시켜줌.
+                mMediaPlayer = null
+            }
+        }
+    }
 
 }
+
